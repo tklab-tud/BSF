60900427d9ab0948ead9bd1b8fd0f822a8256b5e
leonboeck <leonboeck@gmail.com>
2019-10-22 13:49:52 +0200

    Added functionality to record communication traces as csv file.

--- src/routers/basicrouter/BasicRouter.cc
+++ src/routers/basicrouter/BasicRouter.cc
@@ -10,13 +10,13 @@ Define_Module(BasicRouter);
 void BasicRouter::initialize() {
     record_trace = par("record_trace");
     start_recording_trace = par("start_recording_trace");
-    int runNumber = par("run_number"); //EV.getConfigEx()->getActiveRunNumber();//par("runid");
+    int runNumber = par("run_number");
 
     if (record_trace) {
         std::stringstream ss;
         ss << "./traces/NetworkTrace" << runNumber << ".txt";
         opp_string file = ss.str().c_str();
-        dumpFile.open(file.buffer(), std::fstream::out); //will be located in folder 'results/NetworkTrace' of OverSim
+        dumpFile.open(file.buffer(), std::fstream::out);
     }
 }
 
@@ -28,5 +28,10 @@ BasicRouter::~BasicRouter() {
 
 void BasicRouter::handleMessage(cMessage *msg) {
     BasicNetworkMsg* n_msg = static_cast<BasicNetworkMsg*>(msg);
+
+    if(record_trace && simTime() > start_recording_trace){
+        dumpFile << "Src: " << n_msg->getSrcNode() << ", Dst: " << n_msg->getDstNode() << ", Type: " << n_msg->getType() << ", Time: " << simTime() << endl;
+    }
+
     send(n_msg, gate("nodes_out", n_msg->getDstNode()));
 }
